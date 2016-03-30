71b64928403a427c33b62918eb307345bfd7c174
sunilvp <vip.sunil@gmail.com>
2016-03-30 12:06:23 +0530

    Date Gui support added .
    Pending item search based on date.

--- app/src/main/java/com/example/suvp/shop/Activity/SearchInvoiceActivity.java
+++ app/src/main/java/com/example/suvp/shop/Activity/SearchInvoiceActivity.java
@@ -1,5 +1,6 @@
 package com.example.suvp.shop.Activity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +11,14 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.suvp.shop.Adapters.CustomInvoiceListAdapter;
 import com.example.suvp.shop.Adapters.CustomProductItemListAdapter;
+import com.example.suvp.shop.Fragments.DatePickerFragment;
 import com.example.suvp.shop.General.Utility;
 import com.example.suvp.shop.R;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
@@ -25,8 +28,10 @@ import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.Where;
 
 import java.sql.SQLException;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.TimeZone;
 
 import DataBase.ManagedObjects.Invoice;
 import DataBase.ManagedObjects.Item;
@@ -38,12 +43,18 @@ import DataBase.Util.OrmLiteDbHelper;
 public class SearchInvoiceActivity extends FragmentActivity
 {
     CheckBox invoiceNumberCheckBox;
+    CheckBox dateCheckBox;
     TextView serialNumberText;
     Button searchButton;
+    Button startDateSelectButton;
+    Button endDateSelectButton;
 
     private final Context context = this;
     private OrmLiteDbHelper dbHelper_;
     CustomInvoiceListAdapter customListAdapter;
+
+    Calendar selectedStartCalendar;
+    Calendar selectedEndCalendar;
 
     public final static String SERIALIZED_INVOICE = "invoicePassed";
     private final String LOG_TAG = getClass().getSimpleName();
@@ -60,12 +71,16 @@ public class SearchInvoiceActivity extends FragmentActivity
     private void initialize()
     {
         invoiceNumberCheckBox = (CheckBox)findViewById(R.id.checkBoxInvoiceNumber);
+        dateCheckBox = (CheckBox)findViewById(R.id.checkBoxInvoiceDate);
         serialNumberText = (TextView)findViewById(R.id.editTextSearchInvoiceNumber);
         searchButton = (Button)findViewById(R.id.buttonSearchInvoice);
+        startDateSelectButton = (Button)findViewById(R.id.buttonSelectStartDate);
+        endDateSelectButton = (Button)findViewById(R.id.buttonSelectEndDate);
 
         setListActionListener();
 
         enableDisableInvoiceNumberSearch(false);
+        enableDisableDateButtons(false);
 
         invoiceNumberCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -74,13 +89,27 @@ public class SearchInvoiceActivity extends FragmentActivity
             }
         });
 
+        dateCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                enableDisableDateButtons(isChecked);
+            }
+        });
+
         setSearchButtonActionListener();
+        setDateButtonActionListener();
     }
 
     private void enableDisableInvoiceNumberSearch(Boolean aInOption)
     {
         invoiceNumberCheckBox.setChecked(aInOption);
         serialNumberText.setEnabled(aInOption);
+    }
+
+    private void enableDisableDateButtons(Boolean aInOption)
+    {
+        startDateSelectButton.setEnabled(aInOption);
+        endDateSelectButton.setEnabled(aInOption);
     }
 
     private void setSearchButtonActionListener()
@@ -161,6 +190,94 @@ public class SearchInvoiceActivity extends FragmentActivity
                 context.startActivity(singleInvoiceViewActivity);
             }
         });
+    }
+
+
+    private void setDateButtonActionListener()
+    {
+        selectedStartCalendar = Calendar.getInstance(TimeZone.getTimeZone("IST"));
+        selectedEndCalendar = Calendar.getInstance(TimeZone.getTimeZone("IST"));
+
+        startDateSelectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker(onStartdate);
+            }
+        });
+
+        endDateSelectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker(onEndDate);
+            }
+        });
+
+        TextView lStartDateSelectedTextView = (TextView)findViewById(R.id.textFieldStartDate);
+        TextView lEndDateSelectedTextView = (TextView)findViewById(R.id.textFieldEndDate);
+
+        StringBuffer lStartSelectedDate = new StringBuffer();
+        lStartSelectedDate.append(selectedStartCalendar.get(Calendar.DAY_OF_MONTH)).append(":").
+                append(selectedStartCalendar.get(Calendar.MONTH)+1).append(":").
+                append(selectedStartCalendar.get(Calendar.YEAR));
+        lStartDateSelectedTextView.setText(lStartSelectedDate);
+
+        StringBuffer lEndSelectedDate = new StringBuffer();
+        lEndSelectedDate.append(selectedEndCalendar.get(Calendar.DAY_OF_MONTH)).append(":").
+                append(selectedEndCalendar.get(Calendar.MONTH)+1).append(":").
+                append(selectedEndCalendar.get(Calendar.YEAR));
+        lEndDateSelectedTextView.setText(lEndSelectedDate);
+    }
+
+    //The listener for the datepicker fragment or the dialog
+    DatePickerDialog.OnDateSetListener onStartdate = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth)
+        {
+            TextView lDateSelectedTextView = (TextView)findViewById(R.id.textFieldStartDate);
+            StringBuffer lSelectedDate = new StringBuffer();
+            lSelectedDate.append(dayOfMonth).append(":").append(monthOfYear+1).append(":").append(year);
+            lDateSelectedTextView.setText(lSelectedDate);
+
+            selectedStartCalendar = Calendar.getInstance(TimeZone.getTimeZone("IST"));
+            selectedStartCalendar.set(year, monthOfYear, dayOfMonth);
+        }
+    };
+
+    //The listener for the datepicker fragment or the dialog
+    DatePickerDialog.OnDateSetListener onEndDate = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth)
+        {
+            TextView lDateSelectedTextView = (TextView)findViewById(R.id.textFieldEndDate);
+            StringBuffer lSelectedDate = new StringBuffer();
+            lSelectedDate.append(dayOfMonth).append(":").append(monthOfYear+1).append(":").append(year);
+            lDateSelectedTextView.setText(lSelectedDate);
+
+            selectedEndCalendar = Calendar.getInstance(TimeZone.getTimeZone("IST"));
+            selectedEndCalendar.set(year, monthOfYear, dayOfMonth);
+        }
+    };
+
+    //whenever a datepicker dialog is created we have to pass the callback date picked object also so that the date is received back.
+    private void showDatePicker(DatePickerDialog.OnDateSetListener aInListener) {
+        DatePickerFragment date = new DatePickerFragment();
+        /**
+         * Set Up Current Date Into dialog
+         */
+        Calendar calender = Calendar.getInstance();
+        calender.getTime();
+        Bundle args = new Bundle();
+        args.putInt("year", calender.get(Calendar.YEAR));
+        args.putInt("month", calender.get(Calendar.MONTH));
+        args.putInt("day", calender.get(Calendar.DAY_OF_MONTH));
+        date.setArguments(args);
+        /**
+         * Set Call back to capture selected date
+         */
+        date.setCallBack(aInListener);
+        date.show(getSupportFragmentManager(), "Date Picker");
     }
 
     @Override
